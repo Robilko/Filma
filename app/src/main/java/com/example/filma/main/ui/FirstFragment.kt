4f026a05007be351acc1992e4589b38468962cf0
Robilko <sasha.robson@gmail.com>
2022-09-01 23:24:44 +0300

    Настроена предварительная навигация по фрагментам будущих экранов.

--- app/src/main/java/com/example/filma/main/ui/FirstFragment.kt
+++ app/src/main/java/com/example/filma/main/ui/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.example.filma
+package com.example.filma.main.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
+import com.example.filma.R
 import com.example.filma.databinding.FragmentFirstBinding
 
 
